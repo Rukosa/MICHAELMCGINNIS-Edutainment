e02f8d59dc03a9f6684b59abe4aa9f051c3b619f
Rukosa <mwmcginnis490@gmail.com>
2022-03-02 14:24:40 -0800

    Initial Commit

---
+++ Shared/MICHAELMCGINNIS_EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_EdutainmentApp.swift
+//  Shared
+//
+//  Created by Michael Mcginnis on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
